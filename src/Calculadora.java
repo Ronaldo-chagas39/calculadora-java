b9420a0fc7192a349533a89e7bacb1d1beeba46d
Alerrandro <ronaldochagas20194k@gmail.com>
2025-07-11 20:12:46 -0300

    Segundo commit

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        //krlll tou descontrolado 
         System.out.print("Digite o primeiro número: ");
         double n1 = sc.nextDouble();
 
